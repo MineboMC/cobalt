1140df171a1ea246c629790d805a4ace1f21102a
Ian <ianrichb52@outlook.com>
2025-05-21 23:37:47 -0500

    Add ServerUtil

---
+++ src/main/java/net/minebo/cobalt/util/ServerUtil.java
@@ -0,0 +1,33 @@
+package net.minebo.cobalt.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+import java.lang.reflect.Field;
+
+public class ServerUtil {
+
+    private static String getColoredTPS() {
+        double tps = getTPS();
+
+        return ((tps > 18.0)
+                ? ChatColor.GREEN
+                : ((tps > 16.0)
+                ? ChatColor.YELLOW
+                : ChatColor.RED)
+        ) + ((tps > 20.0) ? "*" : "") + Math.min(Math.round(tps * 100.0) / 100.0, 20.0);
+    }
+
+    public static double getTPS() {
+        try {
+            Class<?> serverClass = Class.forName("net.minecraft.server.MinecraftServer");
+            Object server = serverClass.getMethod("getServer").invoke(null);
+            Field tpsField = serverClass.getField("recentTps");
+            return ((double[]) tpsField.get(server))[0];
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1.0;
+        }
+    }
+
+}
