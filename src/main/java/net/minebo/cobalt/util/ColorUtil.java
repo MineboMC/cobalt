d08b503eb90d7c64eb4a2840ec3d78f361107df7
kaboomb52 <ianrichb52@outlook.com>
2025-06-24 16:35:59 -0500

    Add ColorUtil.

---
+++ src/main/java/net/minebo/cobalt/util/ColorUtil.java
@@ -0,0 +1,25 @@
+package net.minebo.cobalt.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ColorUtil {
+
+    public static String translateHexColors(String message) {
+        Pattern hexPattern = Pattern.compile("#([A-Fa-f0-9]{6})");
+        Matcher matcher = hexPattern.matcher(message);
+        StringBuffer buffer = new StringBuffer();
+
+        while (matcher.find()) {
+            String hex = matcher.group(1);
+            StringBuilder replacement = new StringBuilder("§x");
+            for (char c : hex.toCharArray()) {
+                replacement.append('§').append(c);
+            }
+            matcher.appendReplacement(buffer, replacement.toString());
+        }
+        matcher.appendTail(buffer);
+        return buffer.toString();
+    }
+
+}
